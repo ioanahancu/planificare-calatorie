680a053e46ac6a50cdb4cc59a5770ccaa4841850
Ioana Hancu <anda-ioana.hancu@student.tuiasi.ro>
2020-04-26 22:32:23 +0300

    Adaugare continut de start

---
+++ continut/js/script.js
@@ -0,0 +1,26 @@
+function schimbaContinut(resursa, jsFisier=null, jsFunctie=null)
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status==200){
+            document.getElementById("continut").innerHTML= this.responseText;
+            if (jsFisier) {
+                var elementScript = document.createElement('script');
+                elementScript.onload = function () {
+                    console.log("hello");
+                    if (jsFunctie) {
+                        window[jsFunctie]();
+                    }
+                };
+                elementScript.src = jsFisier;
+                document.head.appendChild(elementScript);
+            } else {
+                if (jsFunctie) {
+                    window[jsFunctie]();
+                }
+            }
+        }
+    };
+    xhttp.open("GET", resursa + '.html', true);
+    xhttp.send();
+}
